1c737007f76f1a50dd4086dc23fa53f2c2dd4bc2
Surfer12 <ryanoatsie@outlook.com>
2024-09-26 23:40:48 -0700

    new

--- app/src/main/java/mergesortgradlev1/MainApplication.java
+++ app/src/main/java/mergesortgradlev1/MainApplication.java
@@ -1,20 +1,8 @@
 package mergesortgradlev1;
 
-import mergesortgradlev1.MergeSortClaudeAlterV1;
-
-import java.util.Arrays;
-
 public class MainApplication {
 
-    /**
-     * Main method to demonstrate the merge sort algorithm.
-     *
-     * @param args Command line arguments (not used).
-     */
     public static void main(String[] args) {
-        int[] array = { 38, 27, 43, 3, 9, 82, 10 }; // Example unsorted array
-        System.out.println("Unsorted array: " + Arrays.toString(array));
-        MergeSortClaudeAlterV1.mergeSort(array, 0, array.length - 1);
-        System.out.println("Sorted array: " + Arrays.toString(array));
+        System.out.println("Hello World");
     }
 }
